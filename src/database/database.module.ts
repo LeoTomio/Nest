b405d9e247e5b155b9d37404720d7a8349b200fb
Leo Tomio <leo__tomio@hotmail.com>
2024-06-04 11:29:01 -0300

    Aula 23 -> Configurando o TypeORM com banco de dados Postgres

---
+++ src/database/database.module.ts
@@ -0,0 +1,31 @@
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'docker',
+    database: 'devtraining',
+    entities: [],
+    synchronize: true, // depois que a entidade esta definida no codigo, ela cria automaticamente no banco de dados a coluna 
+
+
+}
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: () => {
+                return {
+                    ...dataSourceOptions
+                }
+            }
+        })
+    ],
+})
+
+export class DatabaseModule { }
